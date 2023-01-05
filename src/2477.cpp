60ac4a7b505a062f490ad863bbdaf17490b0595b
Kestrel-omega <jiho604@naver.com>
2023-01-06 00:25:01 +0900

    20230106

--- src/2477.cpp
+++ src/2477.cpp
@@ -10,15 +10,46 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int K;
-    pair<int, int> length[6];
+    int K, direction, length;
+    int east_west = 0;
+    int north_south = 0;
+    int index = -1;
+    bool check = false;
+    vector<pair<int, int>> direction_length;
 
     cin >> K;
-
-    for(int i = 0; i < 6; i++) cin >> length[i].second >> length[i].first;
-
-    sort(length, length+6);
-    for(int i = 0; i < 6; i++) cout << length[i].first << length[i].second << endl;
     
-    return 0;
+    for (int i = 0; i < 6; i++)
+    {
+        cin >> direction >> length;
+
+        direction_length.push_back(make_pair(direction, length));
+        if (direction >= 3) north_south += length;
+        else east_west += length;
+    }
+
+    north_south /= 2;
+    east_west /= 2;
+
+    for (int i = 0; i < 6; i++)
+    {
+        if (direction_length[i].second == north_south || direction_length[i].second == east_west)
+        {
+            int k = i + 1;
+            if (k > 5) k = 0;
+            if (direction_length[k].second == north_south || direction_length[k].second == east_west)
+            {
+                index = k;
+                break;
+            }
+        }
+    }
+
+    int x = index + 2;
+    int y = index + 3;
+
+    if (x > 5) x -= 6;
+    if (y > 5) y -= 6;
+
+    cout << ((north_south * east_west) - (direction_length[x].second * direction_length[y].second)) * K << "\n";
 }
