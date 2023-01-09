e8b1217f59c65332eb8d7bb8a28e55425fbe07ba
Kestrel-omega <jiho604@naver.com>
2023-01-10 00:17:21 +0900

    modified:   src/15829.cpp

--- src/15829.cpp
+++ src/15829.cpp
@@ -10,25 +10,25 @@ int main()
     cout.tie(NULL);
 
     long long int hash = 0;
-    long long int mod = 1234567891; // 서로소래요
+    long long int mod = 1234567891;
     long long int share = 1;
     int L;
 
     cin >> L;
 
-    char* word = new char[L];
+    char *word = new char[L];
 
     cin >> word;
 
-    for(int i = 0; i < L; i++)
+    for (int i = 0; i < L; i++)
     {
-        hash += (long long int)(((word[i]-('a'-1)) * share) % mod);
+        hash = (hash + (word[i] - ('a' - 1)) * share) % mod;
         share = (share * 31) % mod;
 
         // 출처 : https://velog.io/@lamknh/C-%EB%B0%B1%EC%A4%80-15829-Hashing
     }
 
     cout << hash << endl;
-    
+
     return 0;
 }
