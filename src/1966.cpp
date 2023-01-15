1d9c984f9197ba1983ec1a8ed0588d88c1474cf0
Kestrel-omega <jiho604@naver.com>
2023-01-15 12:47:18 +0900

    20230115

--- src/1966.cpp
+++ src/1966.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -10,23 +9,44 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int T, N, M;
-    cin >> T;
+    int count_print = 0;
+    int T, N, M, priority;
 
+    cin >> T;
     while(T--)
     {
         cin >> N >> M;
-        vector<int> q;
-        while(N--)
-        {
-            int x;
-            cin >> x;
-            q.push_back(x);
-        }
-        int mem_document = q[M];
-        sort(q.begin(), q.end());
+        priority = 0;
+        count_print = 0;
+        queue<pair<int, int>> q;
+        priority_queue<int> pq;
 
-        
+        for(int i = 0; i < N; i++)
+        {
+            int temp;
+            cin >> temp;
+            q.push(make_pair(i, temp));
+            pq.push(temp);
+        }
+
+        while(!q.empty())
+        {
+            int index = q.front().first;
+            int value = q.front().second;
+            q.pop();
+
+            if(pq.top() == value)
+            {
+                pq.pop();
+                count_print++;
+                if(index == M)
+                {
+                    cout << count_print << '\n';
+                    break;
+                }
+            }
+            else q.push(make_pair(index, value));
+        }
     }
     
     return 0;
