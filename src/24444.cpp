c9a17fe85680a1b917a026810163841c64284cc1
Kestrel-omega <jiho604@naver.com>
2023-02-15 20:18:30 +0900

    20230215

--- src/24444.cpp
+++ src/24444.cpp
@@ -10,6 +10,48 @@ int main()
     ios_base ::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+
+    int N, M, R;
+    int result[100001] = {0, };
+    int cnt = 0;
+    bool is_visited[100001] = {false, };
+    vector<int> graph[100001];
+    queue<int> que;
+
+    cin >> N >> M >> R;
+
+    for (int i = 0; i < M; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    for(int i = 1; i <= N; i++) sort(graph[i].begin(), graph[i].end());
+
+    que.push(R);
+    is_visited[R] = true;
+    cnt++;
+    result[R] = cnt;
+    while(!que.empty())
+    {
+        int cur = que.front();
+        que.pop();
+        for(int i = 0; i < graph[cur].size(); i++)
+        {
+            int next = graph[cur][i];
+            if(!is_visited[next])
+            {
+                cnt++;
+                result[next] = cnt;
+                que.push(next);
+                is_visited[next] = true;
+            }
+        }
+    }
+
+    for(int i = 1; i <= N; i++) cout << result[i] << '\n';
     
     return 0;
 }
